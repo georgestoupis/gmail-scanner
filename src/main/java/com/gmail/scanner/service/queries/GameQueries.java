9b8b355f077493ffa96ca0a68d24c1067b639336
gstoupis <gstoupis@gmail.com>
2023-10-23 13:07:19 +0300

    Fix STEAM email subject query

--- src/main/java/com/gmail/scanner/service/queries/GameQueries.java
+++ src/main/java/com/gmail/scanner/service/queries/GameQueries.java
@@ -7,7 +7,7 @@ public final class GameQueries {
 
   public static final Map<Source, String> SOURCE_QUERIES_MAP = Map.of(
       Source.RIOT, "from:noreply@mail.accounts.riotgames.com AND subject:Purchase Confirmation Receipt",
-      Source.STEAM, "from:noreply@steampowered.com AND subject:Thank you for your Steam purchase",
+      Source.STEAM, "from:noreply@steampowered.com AND subject:\"Thank you for your Steam purchase\"",
       Source.KINGUIN, "(from:helpdesk@kinguin.net OR from:help@kinguin.net) AND subject:We have received your new order",
       Source.PLAYSTATION, "from:playstation.com AND subject:Thank You For Your Purchase");
 
