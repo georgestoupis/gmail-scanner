29a7615cdad974e72d2aaf55e169a40399ba588b
gstoupis <gstoupis@gmail.com>
2026-05-02 02:54:06 +0300

    Add XBOX source with the default (fallback) parser

--- src/main/java/com/gmail/scanner/service/model/Source.java
+++ src/main/java/com/gmail/scanner/service/model/Source.java
@@ -1,16 +1,17 @@
 package com.gmail.scanner.service.model;
 
+import com.gmail.scanner.service.parser.OrderParser;
+import com.gmail.scanner.service.parser.fallback.FallbackParser;
 import com.gmail.scanner.service.parser.food.BoxOrderParser;
 import com.gmail.scanner.service.parser.food.EfoodOrderParser;
+import com.gmail.scanner.service.parser.food.WoltOrderParser;
 import com.gmail.scanner.service.parser.games.KinguinOrderParser;
 import com.gmail.scanner.service.parser.games.NintendoOrderParser;
-import com.gmail.scanner.service.parser.OrderParser;
 import com.gmail.scanner.service.parser.games.PlaystationOrderParser;
 import com.gmail.scanner.service.parser.games.RiotOrderParser;
-import com.gmail.scanner.service.parser.shopping.SkroutzOrderParser;
 import com.gmail.scanner.service.parser.games.SteamOrderParser;
+import com.gmail.scanner.service.parser.shopping.SkroutzOrderParser;
 import com.gmail.scanner.service.parser.travel.UberOrderParser;
-import com.gmail.scanner.service.parser.food.WoltOrderParser;
 
 public enum Source {
   EFOOD(new EfoodOrderParser()),
@@ -22,7 +23,8 @@ public enum Source {
   PLAYSTATION(new PlaystationOrderParser()),
   SKROUTZ(new SkroutzOrderParser()),
   UBER(new UberOrderParser()),
-  NINTENDO(new NintendoOrderParser());
+  NINTENDO(new NintendoOrderParser()),
+  XBOX(new FallbackParser());
 
   final OrderParser parser;
 
